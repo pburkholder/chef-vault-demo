fb955de894f1fd2867a2ad5a1265a9230eaa5175
Peter Burkholder <pburkholder@chef.io>
2016-02-01 17:48:47 -0500

    Works with clear authentcation

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 's3cmd'
+
+directory '/home/ubuntu'
+
+file '/home/ubuntu/.s3cfg' do
+  content (
+'
+[default]
+access_key = AKIAIEL734BZ7Y2IHAFQ
+secret_key = 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
+'
+  )
+end
+
+execute 'run_s3cmd' do
+  command 's3cmd get -c /home/ubuntu/.s3cfg s3://chef-vault-demo/minikitten.png'
+  creates '/home/ubuntu/minikitten.png'
+  cwd '/home/ubuntu'
+  action :run
+end
