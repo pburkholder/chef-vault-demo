d3128ed692f5689f9995b0f99cdc198a06d62318
Peter Burkholder <pburkholder@chef.io>
2016-02-01 16:17:59 -0500

    Dokken framework works

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,21 +1,9 @@
 require 'inspec'
 
-control 'motd' do
-  impact 1.0
-  title 'Set MOTD to announce this is a Chef Manage System'
-  desc 'This node is managed by Chef and should not be logged into.'
+# s3cmd package installed
 
-  # s3cmd package installed
+# /home/ubuntu/.s3cfg contains AKIA
 
-  # /home/ubuntu/.s3cfg contains AKIA
+# command s3cmd fetches something
 
-  # command s3cmd fetches something
-
-  # minikitten.png is in place?
-
-  describe file('/etc/motd') do
-    it { should exist }
-    it { should be_file }
-    its(:content) { should match(/Chef Managed System/) }
-  end
-end
+# minikitten.png is in place?
