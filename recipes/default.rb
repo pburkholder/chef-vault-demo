c6222ded44dc5ddb88afc74dc77edb189a59117f
Peter Burkholder <pburkholder@chef.io>
2016-02-02 17:49:07 -0500

    Works with run_action.create

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,24 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+directory('/etc/chef').run_action(:create)
+
+cookbook_file '/etc/chef/encrypted_data_bag_secret' do
+  source 'encrypted_data_bag_secret'
+  owner 'root'
+  group 'root'
+  mode 00006
+end.run_action(:create)
+
+aws = data_bag_item(
+  'encrypted', 'aws', IO.read('/etc/chef/encrypted_data_bag_secret')
+)
+aws_secret_key = aws['aws_secret_key']
+aws_access_key = aws['aws_access_key']
+
 package 's3cmd'
 
 directory '/home/ubuntu'
-
-aws = data_bag_item('cleartext', 'aws')
-aws_secret_key = aws['aws_secret_key']
-aws_access_key = aws['aws_access_key']
 
 template '/home/ubuntu/.s3cfg' do
   source 's3cfg.erb'
